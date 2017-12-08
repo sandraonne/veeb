7734ad4185757a0245f6e48301f763bc3101275a
Sandra Õnne <sandra.onne@khk.ee>
2017-12-08 09:27:39 +0200

    Kirjeldatud kommentaaride kasutamine

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Sandra
+ * Date: 24.11.2017
+ * Time: 15:49
+ */
+
+# see on üherealine kommentaar
+// see on ka üherealine kommentaar
+/**
+ * see on
+ * mitmerealine
+ * kommentaar
+ */
+?>
