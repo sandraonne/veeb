b5076d2aecee72c53c966847b445c048b3c3b7bb
Sandra Õnne <sandra.onne@khk.ee>
2017-12-14 14:57:37 +0200

    Parandatud muutuja dünaamiline loomine

--- funktsioonid.php
+++ funktsioonid.php
@@ -10,10 +10,10 @@ function tabel ($sona1, $sona2, $sona3, $sona4){
         for ($rida = 1; $rida <= 4; $rida++) {
             echo '<tr>';
                 echo '<td>';
-                    echo ${'sona'}.$rida;
+                    echo ${'sona'.$rida};
                 echo '</td>';
             echo '</tr>';
         }
     echo '</table>';
 }
-tabel('1', '2', '3', '4');
+tabel('Kohe', 'saab', 'koju', 'minna');
