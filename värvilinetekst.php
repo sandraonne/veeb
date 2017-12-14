4ba85fb25d46b1e28f27bdb9cf1d641d335bbdda
Sandra Õnne <sandra.onne@khk.ee>
2017-12-14 13:51:10 +0200

    Väljastatud värviline tekst

--- värvilinetekst.php
+++ värvilinetekst.php
@@ -5,8 +5,17 @@
  * Date: 14.12.2017
  * Time: 13:35
  */
-for ($kord = 1; $kord < 6; $kord++){
-    echo 'Värviline tekst<br/ >';
+for ($rida = 1; $rida < 6; $rida++){
+    $varv = '#';
+    for ($kord = 1; $kord < 7; $kord++){
+        $juhuarv = rand(0, 15);
+        $symbol = dechex($juhuarv);
+        $varv = $varv.$symbol;
+    }
+    echo '<font color="'.$varv.'">'.
+    'Värviline tekst'.
+    '</font>'.
+    '<br/ >';
 }
 
 ?>
