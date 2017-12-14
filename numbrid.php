9d54b4c95378b0a2788b096bd0f4d2df631816be
Sandra Õnne <sandra.onne@khk.ee>
2017-12-14 14:29:25 +0200

    Käsitletud while ja do tsüklid koos break ja continue'ga

--- numbrid.php
+++ numbrid.php
@@ -11,8 +11,9 @@ for ($arv = 1; $arv <=10; $arv++) {
     } else {
         $varv = 'red';
     }
-    echo '<font color="'.$varv.'">'.$arv.'</font><br />';
+    echo '<font color="'.$varv.'">'.$arv.'</font>';
 }
+echo '<br/>';
 
 $a = 5; $b = 2;
 $c = $a++;
@@ -29,4 +30,19 @@ echo 'a = '.$a.'<br />';
 echo 'b = '.$b.'<br />';
 echo 'c = '.$c.'<br />';
 echo 'd = '.$d.'<br />';
+
+$kord = 1;
+while ($kord <= 5){
+    echo '<i>'.$kord++.'</i><br />';
+    if($kord == 3) {
+        break;
+    }
+}
+$kord = 1;
+do {
+    if($kord == 3) {
+        continue;
+    }
+    echo '<b>'.$kord.'</b><br />';
+} while (++$kord <= 5);
 ?>
