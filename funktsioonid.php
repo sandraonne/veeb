42f743e68c7a03e4cc2424d8b15b6b4edeb19005
Sandra Õnne <sandra.onne@khk.ee>
2017-12-14 14:53:21 +0200

    Loodud väljastussüsteem tabeli näol

---
+++ funktsioonid.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Sandra
+ * Date: 14.12.2017
+ * Time: 14:31
+ */
+function tabel ($sona1, $sona2, $sona3, $sona4){
+    echo '<table border="1">';
+        for ($rida = 1; $rida <= 4; $rida++) {
+            echo '<tr>';
+                echo '<td>';
+                    echo ${'sona'}.$rida;
+                echo '</td>';
+            echo '</tr>';
+        }
+    echo '</table>';
+}
+tabel('1', '2', '3', '4');
