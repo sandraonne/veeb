80c37fe03fc4d548c1f37b37a4e50187bd9e2da7
Sandra Õnne <sandra.onne@khk.ee>
2018-01-25 11:08:27 +0200

    Koostatud veel üks objekt - must tekst

--- tekstitest.php
+++ tekstitest.php
@@ -8,9 +8,7 @@
 require_once 'tekst.php';
 
 // loome teksti objekti
-$minuTekst = new tekst();
-// määrame tekstile sisu
-$minuTekst->maaraTekst('Tere, maailm!');
+$minuTekst = new tekst('Tere, maailm!');
 // vaatame, mis on objekti sees
 echo '<pre>';
 print_r($minuTekst);
@@ -18,3 +16,9 @@ echo '</pre>';
 
 // prindime välja
 $minuTekst->prindiTekst();
+
+// loome veel ühe objekti ja määrame sisu
+$mustTekst = new tekst('Must tekst');
+echo '<pre>';
+print_r($mustTekst);
+echo '</pre>';
