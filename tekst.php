8b55443fafb381d8e45c8d274f497627aa37527c
Sandra Õnne <sandra.onne@khk.ee>
2018-01-25 10:45:49 +0200

    Koostatud teksti töötluse kirjeldus

---
+++ tekst.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Sandra
+ * Date: 25.01.2018
+ * Time: 10:32
+ */
+
+class tekst
+{ // klassi algus
+    // klassi omadused = klassi väljad (fields)
+    // klassi muutuja (variable)
+    var $sone = ''; // teksti kirjeldavad sõnad
+    // klassi tegevused
+    // meetodid - funktsioonid, mis suuudavad seletada, kuidas üksik tegevus sooritatakse
+    function maaraTekst($sone){ //teksti määramine maaraTekst ('Tere maailm')
+        $this->sone = $sone;
+    }
+    function prindiTekst() { // teksti väljastamine
+        echo $this->sone.'<br />';
+    }
+} // klassi lõpp
